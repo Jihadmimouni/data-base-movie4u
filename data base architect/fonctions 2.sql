44cc32975bf44b38d6afa9c5d7a0a0c4560ca336
jihad mimouni <jihadmimouni6@gmail.com>
2023-04-22 01:44:42 +0100

    adding fonctions

--- data base architect/fonctions 2.sql
+++ data base architect/fonctions 2.sql
@@ -15,6 +15,14 @@ UPDATE users SET email = p_email, password = p_password, birthdate = p_birthdate
 COMMIT;
 END;
 
+--creating fonction for getting user info by id
+create or replace function get_user (user_id number) return sys_refcursor as
+user_cursor sys_refcursor;
+BEGIN
+OPEN user_cursor FOR SELECT * FROM users where id = user_id;
+RETURN user_cursor;
+END;
+
 
 create or replace procedure insert_user (p_name in varchar2, p_email in varchar2, p_password in varchar2 , p_birthdate in DATE,p_image BLOB) as 
 user_id number;
@@ -27,20 +35,41 @@ IF im_id IS NULL THEN im_id := 0; END IF;
 INSERT INTO image (id, image) values (im_id + 1, p_image);
 INSERT INTO users (id, name, email, password, birthdate,image_id) values (user_id + 1, p_name, p_email, p_password, p_birthdate,im_id + 1); 
 CREATE User P_NAME IDENTIFIED BY P_PASSWORD;
-create VIEW P_NAME as select * from users where name = p_name;
-grant select on P_NAME to P_NAME;
-grant UPDATE on P_NAME to P_NAME;
+grant create session to P_NAME;
 grant execute on delete_user to P_NAME;
 grant execute on update_user to P_NAME;
+grant execute on get_user to P_NAME;
 commit;
 exception
 END; 
+
+
+--creating fonction for checking if user exist(used in login)
+create or replace function check_user (p_name in varchar2,p_password in varchar2) return number as
+user_id number;
+BEGIN
+SELECT id INTO user_id from users where name = p_name and password = p_password;
+if user_id is null then
+return 0;
+else
+return 1;
+end if;
+END;
+
+
+
+
+
+
+
+
 
 
 
 --creating new user for inserting when first login
 create user newuser IDENTIFIED BY 1234;
 grant EXECUTE on insert_user to newuser;
+grant EXECUTE on check_user to newuser;
 grant create session to newuser;
 
 
