dc5237e725c93ddf4257ffced7e378b98b395545
jihad mimouni <jihadmimouni6@gmail.com>
2023-04-23 07:01:37 +0100

    fixing some bugs

--- data base architect/fonctions 1.sql
+++ data base architect/fonctions 1.sql
@@ -1,3 +1,4 @@
 alter session set "_ORACLE_SCRIPT"=true;  
 create user movie4u identified by test1234;
-grant all privlage to movie4u;
+grant all privileges to movie4u;
+grant create SESSION to movie4u;
--- data base architect/main.py
+++ data base architect/main.py
@@ -1,9 +1,35 @@
 #creating connection to salplus using system()
 import os
 import shutil
-shutil.copy2("data base architect/fonctions 1.sql","./f1")
-shutil.copy2("data base architect/data base.sql","./db")
-shutil.copy2("data base architect/fonctions 2.sql","./f2")
-os.system("sqlplus SYSTEM/1234@localhost:1521/XE as sysdba @f1")
-os.system("sqlplus movie4u/test1234@localhost:1521/XE @db")
-os.system("sqlplus movie4u/test1234@localhost:1521/XE @f2")
+import subprocess
+shutil.copy2("data base architect/fonctions 1.sql","./f1.sql")
+shutil.copy2("data base architect/data base.sql","./db.sql")
+shutil.copy2("data base architect/fonctions 2.sql","./f2.sql")
+
+def run_sqlplus(sqlplus_script):
+
+    """
+    Run a sql command or group of commands against
+    a database using sqlplus.
+    """
+
+    p = subprocess.Popen(['sqlplus','/nolog'],stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+    (stdout,stderr) = p.communicate(sqlplus_script.encode('utf-8'))
+    stdout_lines = stdout.decode('utf-8').split("\n")
+
+    return stdout_lines
+
+script="""
+    connect SYSTEM/1234@localhost:1521/XE as sysdba
+    @"{path1}"
+    connect movie4u/test1234@localhost:1521/XE
+    @"{path2}"
+    @"{path3}"
+    exit
+"""
+
+print(run_sqlplus(script.format(path1=os.path.abspath("f1.sql"),path2=os.path.abspath("db.sql"),path3=os.path.abspath("f2.sql"))))
+os.remove("f1.sql")
+os.remove("db.sql")
+os.remove("f2.sql")
